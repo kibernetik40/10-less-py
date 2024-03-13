18a35be259b4544e2bc4e7328cac20f67bf7d47c
Kibernetik40 <57685289+kibernetik40@users.noreply.github.com>
2024-03-13 20:47:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pandas as pd
+
+# Исходный DataFrame
 import random
-#Импортируем библотеки 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI': lst})
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
 
-# Переводим данные в one hot вид
-data_one_hot = data.apply(lambda x: x.astype('category').cat.codes)
-data_one_hot = pd.concat([data_one_hot, 1 - data_one_hot], axis=1)
-data_one_hot.columns = ['human', 'robot']
+# Получаем уникальные значения столбца 'whoAmI'
+unique_values = data['whoAmI'].unique()
 
-data_one_hot.head()
-#
+# Создаем DataFrame с нулями, где строки - индексы исходного DataFrame,
+# а столбцы - уникальные значения 'whoAmI'
+one_hot_df = pd.DataFrame(0, index=data.index, columns=unique_values)
+
+# Заполняем one-hot DataFrame значениями 1 в соответствии с исходным DataFrame
+for index, value in data['whoAmI'].iteritems():
+    one_hot_df.loc[index, value] = 1
+
+print(one_hot_df)
